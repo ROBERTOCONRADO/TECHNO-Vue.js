c4651d60795fc63abe0bbc287cb52f4e5d1c51d3
Roberto Conrado <robertoconradorc@gmail.com>
2023-02-11 14:02:17 -0300

    techno-filter-fetch

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const vm = new Vue({
   el: "#app",
   data: {
-    produtos: []
+    produtos: [],
+    produto: false,
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+    }
   },
   methods: {
     fetchProdutos() {
@@ -9,6 +15,13 @@ const vm = new Vue({
         .then(r => r.json())
         .then(r => {
           this.produtos = r;
+        })
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(r => {
+          this.produto = r;
         })
     }
   },
