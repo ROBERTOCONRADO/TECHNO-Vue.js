daf8ddcc558da0692e122bf600565be556a17d74
Roberto Conrado <robertoconradorc@gmail.com>
2023-02-13 11:21:30 -0300

    salvando-informacoes-em-localStorage

--- app.js
+++ app.js
@@ -53,9 +53,19 @@ const vm = new Vue({
     },
     removerItem(index) {
       this.carrinho.splice(index, 1);
+    },
+    checarLocalStorage() {
+      if(window.localStorage.carrinho)
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+    }
+  },
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
     }
   },
   created() {
     this.fetchProdutos();
+    this.checarLocalStorage();
   }
 })
